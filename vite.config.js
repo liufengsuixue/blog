20a96a5742daf8059c9e6ef238488c5bbb38254f
jingys <jingyasen@lansoft.com.cn>
2026-02-09 18:21:57 +0800

    修复资源路径问题，添加blog前缀

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // Vite配置文件
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/blog/',
   build: {
     outDir: 'blog',
     assetsDir: 'assets'
